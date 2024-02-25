4841329a20c84329db01c208751ef2484cd367f8
MD Bygid Hasan <bygidhasan6@gmail.com>
2024-02-26 02:04:04 +0600

    Update dataFetch.js

--- app/data/dataFetch.js
+++ app/data/dataFetch.js
@@ -1,17 +1,17 @@
 export async function CategoryData() {
-  const res = await fetch("http://localhost:4000/category");
+  const res = await fetch("https://server-ueul.onrender.com/category");
   const category = await res.json();
   return category;
 }
 
 export async function SubCategoryData() {
-  const res = await fetch("http://localhost:4000/subcategory");
+  const res = await fetch("https://server-ueul.onrender.com/subcategory");
   const subCategory = await res.json();
   return subCategory;
 }
 
 export async function DuaData() {
-  const res = await fetch("http://localhost:4000/dua");
+  const res = await fetch("https://server-ueul.onrender.com/dua");
   const dua = await res.json();
   return dua;
 }
